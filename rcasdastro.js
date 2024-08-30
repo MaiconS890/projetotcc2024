ce9560ca0c1ec2c4550b9f1db24b721ad03b382e
MaiconS890 <contadeestudossoares@gmail.com>
2024-08-29 21:17:02 -0300

    add

--- rcasdastro.js
+++ rcasdastro.js
@@ -1,29 +1,3 @@
-// script.js
-document.getElementById('registrationForm').addEventListener('submit', async function(event) {
-    event.preventDefault();
-
-    const formData = new FormData(this);
-    const data = {};
-    formData.forEach((value, key) => {
-        data[key] = value;
-    });
-
-    try {
-        const response = await fetch('register.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-
-        const result = await response.json();
-        if (response.ok) {
-            alert('Cadastro realizado com sucesso!');
-        } else {
-            alert('Erro no cadastro: ' + result.message);
-        }
-    } catch (error) {
-        console.error('Erro:', error);
-    }
-});
+function confirmar(){
+<a href="http://127.0.0.1:3000/depois.html"></a>
+}
